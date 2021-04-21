3d3d5151d902106cd5c5065539b51026b637ec37
Barbara Carvalho <barbara.carvalho@convenia.com.br>
2021-04-21 12:26:03 -0300

    created models

---
+++ lib/shared/models/question_model.dart
@@ -0,0 +1,9 @@
+import 'package:dev_quiz/shared/models/answer_model.dart';
+
+class QuestionModel {
+  final String title;
+  final List<AnswerModel> answer;
+
+  QuestionModel({required this.title, required this.answer})
+      : assert(answer.length == 4);
+}
