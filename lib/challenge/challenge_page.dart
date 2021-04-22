c2d9d212f6f8b2b2c8f4ac479f75418f6925fb1e
Barbara Carvalho <barbara.carvalho@convenia.com.br>
2021-04-22 10:08:53 -0300

    add back button, from challenge to home

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -15,10 +15,16 @@ class _ChallengePageState extends State<ChallengePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-          preferredSize: Size.fromHeight(60),
+          preferredSize: Size.fromHeight(86),
           child: SafeArea(
             top: true,
-            child: QuestionIndicatorWidget(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                BackButton(),
+                QuestionIndicatorWidget(),
+              ],
+            ),
           )),
       body: QuizWidget(title: "O que o flutter faz em sua totalidade?"),
       bottomNavigationBar: SafeArea(
