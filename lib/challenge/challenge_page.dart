ecb8c12f80858b27df3ec02cd0ef6e6a9c6f4e5d
Barbara Carvalho <barbara.carvalho@convenia.com.br>
2021-04-22 09:38:00 -0300

    create confirm and cancel buttons layout

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,20 @@ class _ChallengePageState extends State<ChallengePage> {
             child: QuestionIndicatorWidget(),
           )),
       body: QuizWidget(title: "O que o flutter faz em sua totalidade?"),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: NextButtonWidget.white(label: "Fácil")),
+              SizedBox(width: 7),
+              Expanded(child: NextButtonWidget.green(label: "Confirmar")),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
